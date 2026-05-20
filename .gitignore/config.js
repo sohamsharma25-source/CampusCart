a3970146debe844af162172f4020996ba08d0cff
Soham <sohamsharma2323@gmail.com>
2026-05-20 12:26:07 +0530

    Config.js and index.html changes

---
+++ .gitignore/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxx3aIYIF6UCOWtK0LB8eDy551PRxXjis",
+    authDomain: "college-marketplace-21800.firebaseapp.com",
+    projectId: "college-marketplace-21800",
+    storageBucket: "college-marketplace-21800.firebasestorage.app",
+    messagingSenderId: "62702138080",
+    appId: "1:62702138080:web:b3531309dbfc198ef85685"
+};
+
+// Expose config so the app can detect whether Firebase was configured
+window.firebaseConfig = firebaseConfig;
